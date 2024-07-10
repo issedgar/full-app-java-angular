c716309f0509b31ca073287cd1e3973f3d8a85f9
edgar.pech <edgar.pech@workiasolutions.com>
2024-07-10 11:00:29 -0600

    add create user

--- backendApp/src/main/resources/data.sql
+++ backendApp/src/main/resources/data.sql
@@ -1,4 +1,5 @@
+DELETE FROM public.clients WHERE username = 'admin';
+
 INSERT INTO public.clients (address, last_name, "name", "password", username)
 VALUES ('test', 'test', 'test', '$2b$10$lGQcU3N2e1mrot17CTeLL.bulo9v/4yLlsJPAj2m7SmXLSFltISb.', 'admin')
---ON CONFLICT (username) DO NOTHING
 ;
